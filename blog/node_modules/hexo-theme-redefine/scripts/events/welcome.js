255b158556e993ab5dcf8ffa1ddaa2049d4c522e
风熏人醉 <zxc66war3@outlook.com>
2024-07-11 10:58:39 +0800

    update hexo-theme-redefine

--- blog/node_modules/hexo-theme-redefine/scripts/events/welcome.js
+++ blog/node_modules/hexo-theme-redefine/scripts/events/welcome.js
@@ -12,22 +12,66 @@ hexo.on("ready", async () => {
     return new Promise((resolve, reject) => {
       https
         .get(
-          `https://redefine-version.ohevan.com/api/info`,
+          `https://redefine-version.ohevan.com/api/v2/info`,
           { timeout: timeout },
           (response) => {
+            if (response.statusCode < 200 || response.statusCode > 299) {
+              logFailedInfo();
+              return reject(
+                new Error(
+                  `Failed to load page, status code: ${response.statusCode}`,
+                ),
+              );
+            }
             let data = "";
             response.on("data", (chunk) => {
               data += chunk;
             });
             response.on("end", () => {
-              const jsonData = JSON.parse(data);
+              try {
+                const jsonData = JSON.parse(data);
 
-              // hexo.log.info(
-              //   `\n===================================================================\n      ______ __  __  ______  __    __  ______                       \r\n     \/\\__  _\/\\ \\_\\ \\\/\\  ___\\\/\\ \"-.\/  \\\/\\  ___\\                      \r\n     \\\/_\/\\ \\\\ \\  __ \\ \\  __\\\\ \\ \\-.\/\\ \\ \\  __\\                      \r\n        \\ \\_\\\\ \\_\\ \\_\\ \\_____\\ \\_\\ \\ \\_\\ \\_____\\                    \r\n         \\\/_\/ \\\/_\/\\\/_\/\\\/_____\/\\\/_\/  \\\/_\/\\\/_____\/                    \r\n                                                               \r\n ______  ______  _____   ______  ______ __  __   __  ______    \r\n\/\\  == \\\/\\  ___\\\/\\  __-.\/\\  ___\\\/\\  ___\/\\ \\\/\\ \"-.\\ \\\/\\  ___\\   \r\n\\ \\  __<\\ \\  __\\\\ \\ \\\/\\ \\ \\  __\\\\ \\  __\\ \\ \\ \\ \\-.  \\ \\  __\\   \r\n \\ \\_\\ \\_\\ \\_____\\ \\____-\\ \\_____\\ \\_\\  \\ \\_\\ \\_\\\\\"\\_\\ \\_____\\ \r\n  \\\/_\/ \/_\/\\\/_____\/\\\/____\/ \\\/_____\/\\\/_\/   \\\/_\/\\\/_\/ \\\/_\/\\\/_____\/\r\n                                                               \r\n  Github: https:\/\/github.com\/EvanNotFound\/hexo-theme-redefine\n     current version v${version}, the latest version is v${jsonData["dist-tags"].latest}\n===================================================================`,
-              // );
+                if (jsonData.status !== "success") {
+                  logFailedInfo();
+                  return reject(
+                    new Error(`Failed to fetch data: ${jsonData.message}`),
+                  );
+                }
 
-              hexo.log.info(
-                `
+                const redefineData = jsonData.data;
+
+                logInfo(redefineData);
+                checkVersionAndCDNAvailability(redefineData);
+                resolve();
+              } catch (error) {
+                logFailedInfo();
+                reject(new Error(`JSON parse failed: ${error.message}`));
+              }
+            });
+          },
+        )
+        .on("error", (error) => {
+          reject(error);
+        });
+    });
+  }
+
+  try {
+    await fetchRedefineInfo();
+  } catch (error) {
+    hexo.log.warn(`Check latest version failed: ${error}`);
+    hexo.locals.set(`cdnTestStatus_bootcdn`, 404);
+    hexo.locals.set(`cdnTestStatus_staticfile`, 404);
+    hexo.locals.set(`cdnTestStatus_cdnjs`, 404);
+    hexo.locals.set(`cdnTestStatus_sustech`, 404);
+    hexo.locals.set(`cdnTestStatus_zstatic`, 404);
+    hexo.locals.set(`cdnTestStatus_npmMirror`, 404);
+  }
+});
+
+function logInfo(data) {
+  hexo.log.info(
+    `
       +======================================================================================+
       |                                                                                      |
       |    _____ _   _ _____ __  __ _____   ____  _____ ____  _____ _____ ___ _   _ _____    |
@@ -36,51 +80,16 @@ hexo.on("ready", async () => {
       |     | | |  _  | |___| |  | | |___  |  _ <| |___| |_| | |___|  _|  | || |\\  | |___    |
       |     |_| |_| |_|_____|_|  |_|_____| |_| \\_\\_____|____/|_____|_|   |___|_| \\_|_____|   |
       |                                                                                      |
-      |                            current v${version}  latest v${jsonData.npmVersion}                             |
+      |                            current v${version}  latest v${data.npmVersion}                             |
       |                  https://github.com/EvanNotFound/hexo-theme-redefine                 |
       +======================================================================================+
                       `,
-              );
+  );
+}
 
-              if (jsonData.npmVersion > version) {
-                hexo.log.warn(
-                  `\x1b[33m%s\x1b[0m`,
-                  `Redefine v${version} is outdated, please update to v${jsonData.npmVersion}!`,
-                );
-              }
-
-              if (jsonData.staticfileCDN) {
-                hexo.log.info(
-                  `\x1b[32m%s\x1b[0m`,
-                  `CDN available: StaticfileCDN`,
-                );
-                hexo.locals.set(`cdnTestStatus_staticfile`, 200);
-              } else {
-                hexo.log.warn(
-                  `\x1b[31m%s\x1b[0m`,
-                  `StaticfileCDN is unavailable yet.`,
-                );
-                hexo.locals.set(`cdnTestStatus_staticfile`, 404);
-              }
-
-              if (jsonData.bootCDN) {
-                hexo.log.info(`\x1b[32m%s\x1b[0m`, `CDN available: BootCDN`);
-                hexo.locals.set(`cdnTestStatus_bootcdn`, 200);
-              } else {
-                hexo.log.warn(
-                  `\x1b[31m%s\x1b[0m`,
-                  `BootCDN CDN is unavailable yet.`,
-                );
-                hexo.locals.set(`cdnTestStatus_bootcdn`, 404);
-              }
-
-              resolve();
-            });
-          },
-        )
-        .on("error", (error) => {
-          hexo.log.info(
-            `
+function logFailedInfo() {
+  hexo.log.info(
+    `
       +======================================================================================+
       |                                                                                      |
       |    _____ _   _ _____ __  __ _____   ____  _____ ____  _____ _____ ___ _   _ _____    |
@@ -93,17 +102,65 @@ hexo.on("ready", async () => {
       |                  https://github.com/EvanNotFound/hexo-theme-redefine                 |
       +======================================================================================+
        `,
-          );
-          reject(error);
-        });
-    });
+  );
+}
+
+function checkVersionAndCDNAvailability(data) {
+  if (data.npmVersion > version) {
+    hexo.log.warn(
+      `\x1b[33m%s\x1b[0m`,
+      `Redefine v${version} is outdated, please update to v${data.npmVersion}!`,
+    );
   }
 
-  try {
-    await fetchRedefineInfo();
-  } catch (error) {
-    hexo.log.warn(`Check latest version failed: ${error}`);
-    hexo.locals.set(`cdnTestStatus_bootcdn`, 404);
+  if (data.npmMirrorCDN) {
+    hexo.log.info(
+      `\x1b[32m%s\x1b[0m`,
+      `CDN available: NPMMirror (Recommended)`,
+    );
+    hexo.locals.set(`cdnTestStatus_npmMirror`, 200);
+  } else {
+    hexo.log.warn(`\x1b[31m%s\x1b[0m`, `NPMMirror CDN is unavailable yet.`);
+    hexo.locals.set(`cdnTestStatus_npmMirror`, 404);
+  }
+
+  if (data.zstaticCDN) {
+    hexo.log.info(`\x1b[32m%s\x1b[0m`, `CDN available: ZStatic`);
+    hexo.locals.set(`cdnTestStatus_zstatic`, 200);
+  } else {
+    hexo.log.warn(`\x1b[31m%s\x1b[0m`, `ZStatic CDN is unavailable yet.`);
+    hexo.locals.set(`cdnTestStatus_zstatic`, 404);
+  }
+
+  if (data.sustechCDN) {
+    hexo.log.info(`\x1b[32m%s\x1b[0m`, `CDN available: SUSTech`);
+    hexo.locals.set(`cdnTestStatus_sustech`, 200);
+  } else {
+    hexo.log.warn(`\x1b[31m%s\x1b[0m`, `SUSTech CDN is unavailable yet.`);
+    hexo.locals.set(`cdnTestStatus_sustech`, 404);
+  }
+
+  if (data.cdnjsCDN) {
+    hexo.log.info(`\x1b[32m%s\x1b[0m`, `CDN available: CDNJS`);
+    hexo.locals.set(`cdnTestStatus_cdnjs`, 200);
+  } else {
+    hexo.log.warn(`\x1b[31m%s\x1b[0m`, `CDNJS CDN is unavailable yet.`);
+    hexo.locals.set(`cdnTestStatus_cdnjs`, 404);
+  }
+
+  if (data.staticfileCDN) {
+    hexo.log.info(`\x1b[32m%s\x1b[0m`, `CDN available: StaticfileCDN`);
+    hexo.locals.set(`cdnTestStatus_staticfile`, 200);
+  } else {
+    hexo.log.warn(`\x1b[31m%s\x1b[0m`, `StaticfileCDN is unavailable yet.`);
     hexo.locals.set(`cdnTestStatus_staticfile`, 404);
   }
-});
+
+  if (data.bootCDN) {
+    hexo.log.info(`\x1b[32m%s\x1b[0m`, `CDN available: BootCDN`);
+    hexo.locals.set(`cdnTestStatus_bootcdn`, 200);
+  } else {
+    hexo.log.warn(`\x1b[31m%s\x1b[0m`, `BootCDN CDN is unavailable yet.`);
+    hexo.locals.set(`cdnTestStatus_bootcdn`, 404);
+  }
+}
